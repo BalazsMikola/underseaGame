251696c51b0bb36c9bbd9c24f4e932f5656c4543
mikola.balazs <mikola.balazs@DEV-NB-0226>
2020-03-20 09:10:18 +0100

    merge fix

--- backend/StrategyGame.Api/Controllers/BuildingController.cs
+++ backend/StrategyGame.Api/Controllers/BuildingController.cs
@@ -1,13 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using StrategyGame.Model.Entities;
-using StrategyGame.Model.Repository;
-<<<<<<<< HEAD:backend/StrategyGame.Api/Controllers/BuildingController.cs
 using StrategyGame.Bll.Interface;
+using StrategyGame.Model.Entities;
 using System.Collections.Generic;
-using System.Text;
-========
-using System.Collections.Generic;
->>>>>>>> 4337e8e276c07692a0b94b80121bf3917e9dcd33:backend/StrategyGame.Dal/BuildingController.cs
 using System.Threading.Tasks;
 
 namespace StrategyGame.Api.Controllers
